2c7941894da040188d261a6867e4aacff4ff51ad
Diamond Rosales <roserosales288@gmail.com>
2021-08-04 00:02:08 -0400

    trying to submit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 (function ($) {
     "use strict";
     
-    // Initiate the wowjs
-    new WOW().init();
+  
     
     
     // Back to top button
@@ -47,80 +46,8 @@
     });
 
 
-    // Testimonials carousel
-    $(".testimonials-carousel").owlCarousel({
-        center: true,
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
-    
-    // Blogs carousel
-    $(".blog-carousel").owlCarousel({
-        autoplay: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
-    
-    // Class filter
-    var classIsotope = $('.class-container').isotope({
-        itemSelector: '.class-item',
-        layoutMode: 'fitRows'
-    });
+}
+(jQuery)}
 
-    $('#class-filter li').on('click', function () {
-        $("#class-filter li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-        classIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
-    
-    // Portfolio filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
 
-    $('#portfolio-filter li').on('click', function () {
-        $("#portfolio-filter li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
-})(jQuery);
 
